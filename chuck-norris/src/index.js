82b1e771ea7c705561a9556c1083134a5f4f2812
sofia Luna <Sofia21luna@gmail.com>
2020-10-07 23:52:24 -0600

    created my reducers

--- chuck-norris/src/index.js
+++ chuck-norris/src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
-import thunk from "react-thunk";
+import thunk from "redux-thunk";
 
+import { reducer } from "./store/reducers";
 
+import App from "./App";
+// import "./styles.css";
 
+// add applyMiddleware to add middleware to our app
 const store = createStore(reducer, applyMiddleware(thunk));
 
 ReactDOM.render(
@@ -18,3 +20,5 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
+
+
