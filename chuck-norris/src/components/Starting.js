0da12aa8a4dd12b8a636958b41727c26dbbab8a8
sofia Luna <Sofia21luna@gmail.com>
2020-10-07 23:23:34 -0600

    created-store

---
+++ chuck-norris/src/components/Starting.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Starting = () => {
+  return (
+    <div>
+      <h1>Welcome to the Chuck Norris Quote's App</h1>
+      <p>
+        The 'facts' are normally absurd hyperbolic claims about Norris'
+        toughness, attitude, virility, sophistication, and masculinity.
+      </p>
+      <p>
+        When you first start you will receive a random fact, or you could also
+        choose your own category.
+      </p>
+      <p>
+        You can choose: animal, career, celebrity, dev, explicit, fashion, food,
+        history, money, movie, music, political, religion, science, sport,
+        travel
+      </p>
+      <h2>Have Fun!</h2>
+    </div>
+  );
+};
+
+export default Starting;
