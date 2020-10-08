8ba666ea74bf19b5a81a9f3ed6c806e328c16964
sofia Luna <Sofia21luna@gmail.com>
2020-10-07 23:29:48 -0600

    created actions

---
+++ chuck-norris/src/actions/index.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+export const FETCH_QUOTES_START = "FETCH_QUOTES_START";
+export const FETCH_QUOTES_SUCCESS = "FETCH_QUOTES_SUCCESS";
+
+export const fetchQuotes = (category) => (dispatch) => {
+    dispatch({type: FETCH_QUOTES_START});
+    axios.get(category)
+    .then((res) => {
+        dispatch({type: FETCH_QUOTES_SUCCESS, payload: res})
+    })
+    .catch((err) => console.log(err));
+};
